a5bdd02979ad6fa7e3d8f76cb91325df2dfb5349
Younis Wahby <shadowahby@gmail.com>
2026-09-22 15:02:16 +0300

    Google Search Verification

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="3ZvgSnOLmZQR4r59xV07zS-BPC5YJX8xz8thGSoecLk" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
